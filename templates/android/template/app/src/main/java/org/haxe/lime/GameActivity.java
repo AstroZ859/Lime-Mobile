7856886ff557030f45931257d9594f735acdebe9
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-09-16 21:10:26 +0500

    Update GameActivity.java

--- templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
+++ templates/android/template/app/src/main/java/org/haxe/lime/GameActivity.java
@@ -334,6 +334,31 @@ public class GameActivity extends SDLActivity {
 
 	}
 
+	public void openFilePicker() {
+	    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+	    intent.setType("*/*");
+	    intent.addCategory(Intent.CATEGORY_OPENABLE);
+	    
+	    try {
+	        startActivityForResult(Intent.createChooser(intent, "Select a File"), FILE_SELECT_CODE);
+	    } catch (android.content.ActivityNotFoundException ex) {
+	        /*
+			* handle case where no file manager is available
+			*/
+	        runOnUiThread(new Runnable() {
+	            @Override
+	            public void run() {
+	                android.widget.Toast.makeText(
+	                    GameActivity.this,
+	                    "No file manager app found. Please install a file manager to select files.",
+	                    android.widget.Toast.LENGTH_LONG
+	                ).show();
+	            }
+	        });
+	        ex.printStackTrace();
+	    }
+	}
+
 
 	public static void openURL (String url, String target) {
 
